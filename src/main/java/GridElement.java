56da8c5b2c1ffadf1a8a9d6ae94c4df78cbfa969
LeKinaSa <50105043+CAMartins@users.noreply.github.com>
2020-04-29 18:21:32 +0100

    Transfered usefull GridComponent and GridElement functions to Entity

--- src/main/java/GridElement.java
+++
@@ -1,9 +0,0 @@
-public abstract class GridElement extends GridComponent {
-    public GridElement(Position pos) {
-        super(pos);
-    }
-
-    public abstract void takeDamage(int damage);
-
-    public abstract boolean isDead();
-}
