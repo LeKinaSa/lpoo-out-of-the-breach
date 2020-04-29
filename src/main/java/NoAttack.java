8acd84c9831f149f19f8de91f150bbd06317eaa9
LeKinaSa <50105043+CAMartins@users.noreply.github.com>
2020-04-29 17:22:46 +0100

    Deleted old Attack Strategies (not longer needed)

---
+++ src/main/java/NoAttack.java
@@ -0,0 +1,15 @@
+import model.DamageMatrix;
+
+public class NoAttack implements AttackStrategy {
+    private int damage;
+
+    public NoAttack() {}
+
+    @Override
+    public DamageMatrix planAttack(Grid grid, Position pos) {
+        return new DamageMatrix();
+    }
+
+    @Override
+    public void attack(Grid grid, Position pos) {}
+}
