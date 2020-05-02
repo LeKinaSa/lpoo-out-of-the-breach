88acbdedc7f289198346cae9f22cc989231c23a3
LeKinaSa <50105043+CAMartins@users.noreply.github.com>
2020-05-02 19:24:35 +0100

    Update Model Class

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -5,16 +5,22 @@ import java.util.List;
 
 public class Model {
     private List<TerrainTile> terrain;
-    private List<Entity> entities;
+    private List<Enemy> enemies;
+    private List<Ally> allies;
+    private List<City> cities;
 
     public Model() {
         this.terrain = new ArrayList<>(64);
-        this.entities = new ArrayList<>(64);
+        this.enemies = new ArrayList<>();
+        this.allies = new ArrayList<>();
+        this.cities = new ArrayList<>();
     }
 
-    public Model(List<TerrainTile> terrain, List<Entity> entities) {
+    public Model(List<TerrainTile> terrain, List<Enemy> enemies, List<Ally> allies, List<City> cities) {
         this.terrain = terrain;
-        this.entities = entities;
+        this.enemies = enemies;
+        this.allies = allies;
+        this.cities = cities;
     }
 
     public boolean hasTerrain(Position pos) {
@@ -26,11 +32,11 @@ public class Model {
     }
 
     public boolean hasEntity(Position pos) {
-        return this.entities.get(pos.getLinearMatrixPosition()) != null;
+        //return this.entities.get(pos.getLinearMatrixPosition()) != null;
     }
 
     public Entity getEntityAt(Position pos) {
-        return this.entities.get(pos.getLinearMatrixPosition());
+        //return this.entities.get(pos.getLinearMatrixPosition());
     }
 
     public void addTerrain(TerrainTile terrainTile, Position pos) {
@@ -44,28 +50,28 @@ public class Model {
 
     public void addEntity(Entity entity) {
         if (!this.hasEntity(entity.getPosition())) {
-            this.entities.set(entity.getPosition().getLinearMatrixPosition(), entity);
+            //this.entities.set(entity.getPosition().getLinearMatrixPosition(), entity);
         }
         else {
             // exception
         }
     }
 
-    public void draw() {
+    /*public void draw() {
         for (TerrainTile terrainTile : this.terrain) {
             //terrainTile.draw(); // TROUBLE
         }
         for (Entity entity : this.entities) {
             entity.draw();
         }
-    }
+    }*/
 
     public void inflictDamage(Position pos, int damage) {
         if (this.hasEntity(pos)) {
             Entity entity = this.getEntityAt(pos);
             entity.takeDamage(damage);
             if (entity.isDead()) {
-                this.entities.remove(entity);
+                //this.entities.remove(entity);
             }
         }
     }
