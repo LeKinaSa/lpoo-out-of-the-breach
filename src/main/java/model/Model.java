be72807b9ea15eb6ad21d4f6e85c47ec66f9288c
Daniel Monteiro <daniel.amaro.monteiro@gmail.com>
2020-05-02 03:52:22 +0100

    Implement entity finding functions

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -39,10 +39,27 @@ public class Model {
     }
 
     public boolean tileOccupied(Position pos) {
-        return true;
+        return getEntityAt(pos) != null;
     }
 
     public Entity getEntityAt(Position pos) {
+        for (Entity i : cities) {
+            if (i.getPosition().same(pos)) {
+                return i;
+            }
+        }
+
+        for (Entity i : allies) {
+            if (i.getPosition().same(pos)) {
+                return i;
+            }
+        }
+
+        for (Entity i : enemies) {
+            if (i.getPosition().same(pos)) {
+                return i;
+            }
+        }
         return null;
     }
 }
