f5df91248034fc9ab0688ec2601cbb65c9409302
LeKinaSa <50105043+CAMartins@users.noreply.github.com>
2020-05-03 14:53:53 +0100

    Position Tests

--- src/test/java/PositionTest.java
+++ src/test/java/PositionTest.java
@@ -32,7 +32,7 @@ public class PositionTest {
         try {
             pos = new Position(3, 4);
         } catch (OutsideOfTheGrid outsideOfTheGrid) {
-            assertEquals(false, true); // TODO
+            assertEquals(false, true);
         }
         assertEquals(3, pos.getX());
         assertEquals(4, pos.getY());
@@ -45,7 +45,7 @@ public class PositionTest {
         try {
             pos = new Position(5, 2);
         } catch (OutsideOfTheGrid outsideOfTheGrid) {
-            assertEquals(false, true); // TODO
+            assertEquals(false, true);
         }
         assertEquals(5, pos.getX());
         assertEquals(2, pos.getY());
@@ -58,7 +58,7 @@ public class PositionTest {
         try {
             pos = new Position(0, 0);
         } catch (OutsideOfTheGrid outsideOfTheGrid) {
-            assertEquals(false, true); // TODO
+            assertEquals(false, true);
         }
         assertEquals(0, pos.getX());
         assertEquals(0, pos.getY());
@@ -71,11 +71,103 @@ public class PositionTest {
         try {
             pos = new Position(7, 7);
         } catch (OutsideOfTheGrid outsideOfTheGrid) {
-            assertEquals(false, true); // TODO
+            assertEquals(false, true);
         }
         assertEquals(7, pos.getX());
         assertEquals(7, pos.getY());
         assertEquals(63, pos.getLinearMatrixPosition());
     }
 
+    @Test
+    public void sameTest() {
+        Position pos = null;
+        try {
+            pos = new Position(2, 3);
+        }
+        catch (OutsideOfTheGrid o) {
+            assertEquals(false, true);
+        }
+        assertEquals(2, pos.getX());
+        assertEquals(3, pos.getY());
+        Position p = null;
+        try {
+            p = new Position(2, 3);
+        }
+        catch (OutsideOfTheGrid o) {
+            assertEquals(false, true);
+        }
+        assertEquals(true, pos.same(p));
+        assertEquals(26, pos.getLinearMatrixPosition());
+    }
+
+    @Test
+    public void distanceToAndSetTest() {
+        Position pos = null;
+        Position p = null;
+        try {
+            pos = new Position(1, 2);
+            p   = new Position(3, 6);
+        }
+        catch (OutsideOfTheGrid o) {
+            assertEquals(false, true);
+        }
+        assertEquals(1, pos.getX());
+        assertEquals(2, pos.getY());
+        assertEquals(3,   p.getX());
+        assertEquals(6,   p.getY());
+
+        assertEquals(6, pos.distanceTo(p));
+
+        pos.setPosition(p);
+        assertEquals(3, pos.getX());
+        assertEquals(6, pos.getY());
+    }
+
+    @Test
+    public void directionsTest1() {
+        Position pos = null;
+        try {
+            pos = new Position(0, 0);
+        }
+        catch (OutsideOfTheGrid o) {
+            assertEquals(false, true);
+        }
+        Position north = pos.north();
+        Position south = pos.south();
+        Position  east = pos.east ();
+        Position  west = pos.west ();
+
+        assertEquals(null, north);
+        assertEquals(null,  west);
+
+        assertEquals(0, south.getX());
+        assertEquals(1, south.getY());
+
+        assertEquals(1,  east.getX());
+        assertEquals(0,  east.getY());
+    }
+
+    @Test
+    public void directionsTest2() {
+        Position pos = null;
+        try {
+            pos = new Position(7, 7);
+        }
+        catch (OutsideOfTheGrid o) {
+            assertEquals(false, true);
+        }
+        Position north = pos.north();
+        Position south = pos.south();
+        Position  east = pos.east ();
+        Position  west = pos.west ();
+
+        assertEquals(null, south);
+        assertEquals(null,  east);
+
+        assertEquals(7, north.getX());
+        assertEquals(6, north.getY());
+
+        assertEquals(6,  west.getX());
+        assertEquals(7,  west.getY());
+    }
 }
