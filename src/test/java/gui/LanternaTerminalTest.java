90245b4293c7b0c9095e2939183f95b3af013d3e
Daniel Monteiro <daniel.amaro.monteiro@gmail.com>
2020-05-03 21:31:58 +0100

    Add LanternaTerminal test

--- src/test/java/gui/LanternaTerminalTest.java
+++ src/test/java/gui/LanternaTerminalTest.java
@@ -7,21 +7,37 @@ import com.googlecode.lanterna.terminal.virtual.DefaultVirtualTerminal;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import out_of_the_breach.GUI.LanternaTerminal;
 
+
+import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class LanternaTerminalTest {
+    private LanternaTerminal lterm;
     private Terminal term;
 
     @Before
-    public void initBuffer() {
-        new Mockito();
+    public void initBuffer() throws IOException {
         term = new DefaultVirtualTerminal();
+        lterm = new LanternaTerminal(term);
     }
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void testNullKeystroke() throws IOException {
+        assertNull(lterm.pollInput()); // Would be great if we could inject keystrokes:/
+    }
+
+    @Test
+    public void testBuffer() throws IOException {
+        lterm.clear();
+        TextGraphics buffer = lterm.getTerminalBuffer();
+        buffer.setCharacter(0, 0, 'c');
+        lterm.refresh();
+
+        buffer = lterm.getTerminalBuffer();
+        assertEquals(buffer.getCharacter(0, 0).getCharacter(), 'c');
     }
 }
